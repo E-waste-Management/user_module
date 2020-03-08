6d14fa39da793821f2532f91709b9dee2187d195
princystalin <princystalinnadar123@gmail.com>
2020-03-08 15:12:38 +0530

    new addtitions

---
+++ app/src/main/java/com/example/princy/user_module/order.java
@@ -0,0 +1,15 @@
+package com.example.princy.user_module;
+
+public class order {
+    private String product, images, images1,images2,quantity;
+
+    public order(String product, String images, String images1,String images2,String quantity) {
+
+        product = product;
+        images = images;
+        images1 = images1;
+        images2 = images2;
+        quantity = quantity;
+
+    }
+    }
